859e9c1faa46ec32027202ae89a427c30877d92c
Martijn van Iersel <mvaniersel@gmail.com>
2026-06-04 18:39:44 +0200

    PWA, Manifest, Workbox

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
+/* eslint-disable camelcase */
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
 	build: {
@@ -12,4 +14,40 @@ export default defineConfig({
 		},
 	},
 	base: './', // Use relative paths in index.html, makes our app relocatable.
+	plugins: [
+		VitePWA({
+			registerType: 'autoUpdate',
+			// cache all the imports
+			workbox: {
+				globPatterns: [ '**/*' ],
+				maximumFileSizeToCacheInBytes: 10000000,
+			},
+			// cache all the static assets in the public folder
+			includeAssets: [
+				'**/*',
+			],
+			manifest: {
+				name: 'Triggies Attack!',
+				short_name: 'triggies',
+
+				// URL relative to manifest file
+				start_url: './',
+
+				display: 'standalone',
+				background_color: '#42423b',
+				theme_color: '#f33061',
+				icons: [ {
+					// relative to manifest file
+					src: './logo192.png',
+					sizes: '192x192',
+					type: 'image/png',
+				}, {
+					// relative to manifest file
+					src: './logo512.png',
+					sizes: '512x512',
+					type: 'image/png',
+				} ],
+			},
+		}),
+	],
 });
